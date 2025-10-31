02c384b6049e86432f5bcfc3fbb72667d9a91d01
Ali Ahangar <aliahangar80@gmail.com>
2025-10-31 16:34:54 +0330

    Initial commit for cows_and_bulls_game project

---
+++ cows_and_bulls_game.py
@@ -0,0 +1,39 @@
+import random
+
+def generate_secret():
+    digits = list(range(10))
+    random.shuffle(digits)
+    secret = ""
+    for digit in digits[:4]:
+        secret += str(digit)
+    return secret
+
+def calculate_cows_and_bulls(secret, guess):
+    cows = 0
+    bulls = 0
+
+    for i in range(4):
+        if guess[i] == secret[i]:
+            bulls += 1
+        elif guess[i] in secret:
+            cows += 1
+
+    return cows, bulls
+
+def main():
+    secret = generate_secret()
+    while True:
+        guess = input("Guess: ")
+        if len(guess) == 4 and guess.isdigit() and len(set(guess)) == 4:
+            cows, bulls = calculate_cows_and_bulls(secret, guess)
+            print(f"{cows} cows, {bulls} bulls.")
+
+            if bulls == 4:
+                print("Congratulations, you guessed the correct number!")
+                break
+        else:
+            print("Invalid guess. Please enter a 4-digit number with unique digits.")
+
+
+if __name__ == '__main__':
+    main()
